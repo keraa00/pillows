239378edaf3e437f8dbf74c6e52eda19d81299e8
Kerim Smajlovic <kerim6798@gmail.com>
2025-05-18 15:49:12 +0200

    cleaned some junk code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Hero2 from "./components/Hero2";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <Hero /> */}
-      <Hero2 />
+      <Hero />
     </>
   );
 }
