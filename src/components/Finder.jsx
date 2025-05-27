c966897327faa4864b8fe3c3625e582233c7c012
Kerim Smajlovic <kerim6798@gmail.com>
2025-05-27 20:01:53 +0200

    Finder component

--- src/components/Finder.jsx
+++ src/components/Finder.jsx
@@ -1,6 +1,14 @@
 import finderPillow from "../assets/img/finder-pillow.png";
+import finderImg1 from "../assets/img/finder1.png";
+import finderImg2 from "../assets/img/finder2.png";
+import finderImg3 from "../assets/img/finder3.png";
 
 export default function Finder() {
+  const finderImages = [
+    { img: finderImg1, text: "Pillows" },
+    { img: finderImg2, text: "Sheets & Duvet Covers" },
+    { img: finderImg3, text: "Comforters" },
+  ];
   return (
     <>
       <div
@@ -18,6 +26,25 @@ export default function Finder() {
             Let's get started
           </button>
         </div>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-4 mt-16">
+        {finderImages.map((image, index) => (
+          <div
+            key={index}
+            className="relative w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 h-64 cursor-pointer rounded overflow-hidden"
+          >
+            <img
+              src={image.img}
+              alt={`Finder ${index}`}
+              className="w-full h-full object-cover"
+            />
+            <div className="absolute top-2 left-2 px-2 py-1 rounded text-sm text-[#1F3951]">
+              <p className="font-semibold">{image.text}</p>
+              <p className="text-xs underline">Shop now</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
